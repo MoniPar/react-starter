97af85d8206cfe54cac3682d20a30cfc3210501e
Monique <monique.parnis@gmail.com>
2023-08-11 13:32:49 +0100

    Add useState to update State upon user click event

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,30 @@
+import React, { useState } from 'react';
+
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+  
+  console.log('Expense Item is being re-evaluated!')
 
-    return (
-      <Card className="expense-item">
-        <ExpenseDate date={props.date}/>
-        <div className='expense-item__description'>
-          <h2>{props.name}</h2>
-          <div className='expense-item__price'>€{props.amount}</div>
-        </div>
-      </Card>
-    );
+  const [name, setName] = useState(props.name);
+
+  const clickHandler = () => {
+    setName('Updated!');
+    console.log(name);
+  };
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date}/>
+      <div className='expense-item__description'>
+        <h2>{name}</h2>
+        <div className='expense-item__price'>€{props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  );
 }
 
 export default ExpenseItem; 
