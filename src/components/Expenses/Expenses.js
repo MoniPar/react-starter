2fbd4929a328d07bf4dda5acb38ef33df6474e14
Monique <monique.parnis@gmail.com>
2023-08-16 17:57:43 +0100

    Render the expenses array dynamically

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,26 +15,20 @@ const Expenses = (props) => {
     };
 
     return (
-        <Card className="expenses">
-            <ExpensesFilter selected={enteredYear} onSaveExpenseYear={saveExpenseYearHandler} />
-            <ExpenseItem
-                name={props.item[0].title} 
-                amount={props.item[0].amount} 
-                date={props.item[0].date} />
-            <ExpenseItem
-                name={props.item[1].title} 
-                amount={props.item[1].amount} 
-                date={props.item[1].date} />
-            <ExpenseItem
-                name={props.item[2].title} 
-                amount={props.item[2].amount} 
-                date={props.item[2].date} />
-            <ExpenseItem
-                name={props.item[3].title} 
-                amount={props.item[3].amount} 
-                date={props.item[3].date} />
-        </Card>
-    )
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={enteredYear}
+          onSaveExpenseYear={saveExpenseYearHandler}
+        />
+        {props.item.map((expense) => (
+          <ExpenseItem
+            name={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
+    );
 };
 
 export default Expenses;
