c76a163287322efa7be76b0df740b11f92b606bd
Monique <monique.parnis@gmail.com>
2023-08-16 11:31:34 +0100

    Add ExpensesFilter, pass value to Expenses and save state in Expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,22 @@
+import React, { useState } from 'react';
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 import Card from '../UI/Card';
 import "./Expenses.css";
 
 const Expenses = (props) => {
+
+    const [enteredYear, setEnteredYear] = useState('2023');
+
+    const saveExpenseYearHandler = (selectedYear) => {
+        setEnteredYear(selectedYear);
+        // console.log('In Expenses.js')
+        // console.log(selectedYear);
+    };
+
     return (
         <Card className="expenses">
+            <ExpensesFilter selected={enteredYear} onSaveExpenseYear={saveExpenseYearHandler} />
             <ExpenseItem
                 name={props.item[0].title} 
                 amount={props.item[0].amount} 
