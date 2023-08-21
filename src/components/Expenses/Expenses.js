a6e023f8a3a6c6006caf8b4fbb7249741e768af6
Monique <monique.parnis@gmail.com>
2023-08-21 11:21:04 +0100

    Add filter method to render only the expenses corresponding to selected year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,31 +5,35 @@ import Card from '../UI/Card';
 import "./Expenses.css";
 
 const Expenses = (props) => {
+  const [enteredYear, setEnteredYear] = useState("2021");
 
-    const [enteredYear, setEnteredYear] = useState('2023');
+  const saveExpenseYearHandler = (selectedYear) => {
+    setEnteredYear(selectedYear);
+  };
 
-    const saveExpenseYearHandler = (selectedYear) => {
-        setEnteredYear(selectedYear);
-        console.log('In Expenses.js')
-        console.log(selectedYear);
-    };
+  // Filter through the updated expenses array, return the items with a year that matches the one selected in the dropdown
+  const filteredByYear = props.item.filter((expense) => {
+    return expense.date.getFullYear().toString() === enteredYear;
+  });
 
-    return (
-      <Card className="expenses">
-        <ExpensesFilter
-          selected={enteredYear}
-          onSaveExpenseYear={saveExpenseYearHandler}
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={enteredYear}
+        onSaveExpenseYear={saveExpenseYearHandler}
+      />
+      {/* Creates a new array of JSX elements based on the filtered array */}
+      {filteredByYear.map((expense) => (
+        <ExpenseItem
+          // Add key prop to set a unique value per list item so that React can identify individual items
+          key={expense.id}
+          name={expense.title}
+          amount={expense.amount}
+          date={expense.date}
         />
-        {props.item.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            name={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
-      </Card>
-    );
+      ))}
+    </Card>
+  );
 };
 
 export default Expenses;
